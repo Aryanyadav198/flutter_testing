5d19a09f31c92b2896713e5500dbe3b3ce79c304
Aryan yadav <aryanyadav898487@gmail.com>
2025-07-07 00:31:06 +0600

    Added some more test functionality to run the test function at one and before run each function if some thing want to do it is also possible

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -1,36 +1,43 @@
-import 'dart:math';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_testing/src/screens/common_screen/counter.dart';
+// PreTest()
+// setUp((){}); Before test start this run for each of the test
+// setUpAll((){}); Before test start this run only once
 
 void main() {
-  test("verify that the counter value of the counter class is 0", () {
-    // Arrange
-    final Counter counter = Counter();
-    // Act
-    final value = counter.counter;
-    // Assert
-    expect(value, 0);
+  late Counter counter;
+  setUp(() {
+    counter = Counter();
   });
-  test("Verify that the increment function is working correctly expected 1",
-      () {
-    // Arrange
-    final Counter counter = Counter();
-    // Act
-    counter.increment();
-    final value = counter.counter;
-    // Assert
-    expect(value, 1);
-  });
-  test("Verity that the decrement function is working properly expected 0",
-      () {
-        // Arrange 
-        final Counter counter = Counter();
-        // Act 
-        counter.decrement();
-        final value = counter.counter;
-        // Assert
-        expect(value, -1);
 
-      });
+  group("Counter class - ", () {
+    test("verify that the counter value of the counter class is 0", () {
+      // Arrange
+      // Act
+      final value = counter.counter;
+      // Assert
+      expect(value, 0);
+    });
+    test("Verify that the increment function is working correctly expected 1",
+        () {
+      // Arrange
+      // Act
+      counter.increment();
+      final value = counter.counter;
+      // Assert
+      expect(value, 1);
+    });
+    test("Verity that the decrement function is working properly expected 0",
+        () {
+      // Arrange
+      // Act
+      counter.decrement();
+      final value = counter.counter;
+      // Assert
+      expect(value, -1);
+    });
+  });
+  // PostTest
+  // tearDown((){}); This will run after one test complete for each test
+  // tearDownAll((){}); This will run after all the tests complete only once
 }
